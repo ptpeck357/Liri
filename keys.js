e665b81c660a0694544cb7e3f64c7227523d1498
Peter Peck <karatekid4christ@gmail.com>
2017-11-09 14:36:56 -0600

    Setting file structure.

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('keys are loaded');
+
+var twitterKeys = {
+  consumer_key: 'UvOsT9r4IaXIUqG3YwbmUoGmU',
+  consumer_secret: '78leTHMKJbkJOnJHyARMJ7GfWww1kimNxi23GIqqeHksCcwRTq',
+  access_token_key: '716508508548501504-cISn4kxoYzS2aL0MTzlU0lDW1DKr5QO',
+  access_token_secret: '57slkFwZs83W3TsZKKeXZ5n2dxAagpf6AiKo6teWp28CB'
+}
+
+var spotifyKeys = {
+	Client_ID: '17aab725b284462aacfd997301bd07a1',
+	Client_Secret: '5f242864dd51446bbb52a6689191de3e'
+}
+
+module.exports = twitterKeys;
+module.exports = spotifyKeys;
---
+++ liri.js
@@ -0,0 +1,2 @@
+var twitterKeys = require('./keys');
+console.log(twitterKeys);
